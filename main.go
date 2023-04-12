a563e7f04df424152c21e326e858c4258533eb79
bozkayasalihx <jamesgosling10@gmail.com>
2023-04-13 01:48:02 +0300

    performans improvment

--- main.go
+++ main.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"fmt"
+	"github.com/bozkayasalihx/framegoos/util"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"io/fs"
 	"log"
 	"os"
 	"os/exec"
 	"path"
-	// ffmpeg "github.com/u2takey/ffmpeg-go"
+	"sync"
 )
 
 var (
-	list = []string{"pip", "pip3"}
-    limit = 20
+	list          = []string{"pip", "pip3"}
+	limit         = 20
+	outdir        = "./test/output.mp4"
+	backgroundDir = "./test/background.mp4"
+	latestDir     = "./test/latest.mp4"
 )
 
 type Node struct {
-	Prev *Node
-	Next *Node
+	Prev   *Node
+	Next   *Node
 	Values []fs.DirEntry
 }
 
@@ -35,11 +40,11 @@ func NewList() *List {
 
 func (L *List) Insert(vals []fs.DirEntry) {
 	list := &Node{
-		Next: L.Head,
-		Values:  vals,
+		Next:   L.Head,
+		Values: vals,
 	}
 	if L.Head != nil {
-		L.Head.Prev= list
+		L.Head.Prev = list
 	}
 	L.Head = list
 
@@ -47,7 +52,7 @@ func (L *List) Insert(vals []fs.DirEntry) {
 	for l.Next != nil {
 		l = l.Next
 	}
-	L.Tail= l
+	L.Tail = l
 }
 
 func (L *List) Display() {
@@ -89,16 +94,18 @@ func (list *List) Aggrator(path string) {
 		log.Fatalf("couldn't read dir %v", err)
 	}
 	chunk := limit
-    a, b := 0, 0; 
-    length := len(dirs);
+	a, b := 0, 0
+	length := len(dirs)
 
 	for a < length {
-        b = a + chunk; 
-        if b > length {
-            b = length;
-        }
+		b = a + chunk
+		if b > length {
+			b = length
+		}
+		fmt.Println(a)
+		fmt.Println(b)
 		list.Insert(dirs[a:b])
-        a = b;
+		a = b
 	}
 }
 
@@ -117,37 +124,54 @@ func main() {
 	os.Mkdir(inputPath, 0777)
 	os.Mkdir(outputPath, 0777)
 
-	elems := []string{"ffmpeg", "-i", "test/test.mp4", inputPath + "/%04d.png"}
-	commandRunner(elems...)
+	elems := []string{"ffmpeg", "-i", "test/test.mp4", "-vf", "fps=30", inputPath + "/%04d.png"}
+	out, e := commandRunner(elems...)
+	if e != nil {
+		panic(e)
+	}
+	fmt.Println(string(out))
 	list := NewList()
 	list.Aggrator(inputPath)
 
-    c := make(chan int)
-    var total int
-    var behind int
-    
-    for list != nil {
-        head := list.Head 
-        total += len(head.Values);
-        for _, curDir := range head.Values {
-            go func( ){
-                _, e := exec.Command("rembg", "i", inputPath + "/" +  curDir.Name(), outputPath + "/" + curDir.Name()).Output()
-                if e != nil {
-                    panic(err);
-                }
-                c <- 1
-            }()
-        }
-        fmt.Println("processsing...") 
-        behind += <-c;
-        if behind == total {
-            head = head.Next; 
-        }
-    }
+	wg := sync.WaitGroup{}
 
+	fmt.Println(inputPath)
 
-	// err = ffmpeg.Input(fmt.Sprintf("%s/%s", resultPath, "%04d.png"), ffmpeg.KwArgs{"r": "60"}).Output("./test/output.mp4", ffmpeg.KwArgs{"vcodec": "libx264", "crf": 15, "pix_fmt": "yuv420p"}).OverWriteOutput().ErrorToStdOut().Run()
-	// if err != nil {
-	//     log.Fatalf("couldn't ffmpeg build cmd %v" ,err);
-	// }
+	for list != nil {
+		head := list.Head
+		for _, curDir := range head.Values {
+			wg.Add(1)
+			go func(c fs.DirEntry) {
+				fmt.Println("command running")
+				_, e := exec.Command("rembg", "i", inputPath+"/"+c.Name(), outputPath+"/"+c.Name()).Output()
+				if e != nil {
+					panic(err)
+				}
+				fmt.Println("command done...")
+				defer wg.Done()
+			}(curDir)
+		}
+		head = head.Next
+		// break
+	}
+	wg.Wait()
+	fmt.Println("all done")
+
+	err = ffmpeg.Input(fmt.Sprintf("%s/%s", outputPath, "%04d.png"), ffmpeg.KwArgs{"r": "30"}).
+		Output(outdir, ffmpeg.KwArgs{"vcodec": "libx264", "crf": 15, "pix_fmt": "yuv420p"}).
+		OverWriteOutput().ErrorToStdOut().Run()
+	if err != nil {
+		log.Fatalf("couldn't ffmpeg build cmd %v", err)
+	}
+
+	overlay := ffmpeg.Input(outdir).Filter("scale", ffmpeg.Args{"64:-1"})
+	err = ffmpeg.Filter(
+		[]*ffmpeg.Stream{
+			ffmpeg.Input(backgroundDir),
+			overlay,
+		}, "overlay", ffmpeg.Args{"10:10"}, ffmpeg.KwArgs{"enable": "gte(t,1)"}).
+		Output(latestDir).OverWriteOutput().ErrorToStdOut().Run()
+
+	err = util.Cleanup(inputPath)
+	err = util.Cleanup(outputPath)
 }
